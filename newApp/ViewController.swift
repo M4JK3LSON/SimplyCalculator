19dd79c7836bc27716a3f338026a58475c3e0bf8
Michal L <Michal@imac-programista-2.local>
2021-03-01 09:27:19 +0100

    Initial Commit

---
+++ newApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  newApp
+//
+//  Created by Michal L on 01.03.2021.
+//  Copyright © 2021 Michal L. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
